6d050762c9cd7dd9500621f786d36eb16bec5ef4
Ítalo Silveira <italo-silveira@impulso.network>
2020-12-25 19:15:40 -0300

    add test mocks

---
+++ generators/app/templates/src/BAYSOFT.Core.Domain.Services.Tests/Default/MockDefaultHelper.cs
@@ -0,0 +1,26 @@
+﻿using BAYSOFT.Core.Domain.Entities.Default;
+using BAYSOFT.Core.Domain.Interfaces.Infrastructures.Data.Contexts;
+using Microsoft.EntityFrameworkCore;
+using Moq;
+using System.Collections.Generic;
+using System.Linq;
+using BAYSOFT.Core.Domain.Services.Tests;
+
+namespace BAYSOFT.Core.Domain.Services.Tests.Default
+{
+    public static class MockDefaultHelper
+    {
+        internal static Mock<IDefaultDbContext> GetMockedDefaultDbContext()
+        {
+            var mockedDeafultDbContext = new Mock<IDefaultDbContext>();
+
+            return mockedDeafultDbContext;
+        }
+        internal static Mock<IDefaultDbContextQuery> GetMockedDefaultDbContextQuery()
+        {
+            var mockedDeafultDbContextQuery = new Mock<IDefaultDbContextQuery>();
+
+            return mockedDeafultDbContextQuery;
+        }
+    }
+}
---
+++ generators/app/templates/src/BAYSOFT.Core.Domain.Services.Tests/Default/Samples/AddMockedSamplesExtensions.cs
@@ -0,0 +1,54 @@
+﻿using BAYSOFT.Core.Domain.Entities.Default;
+using BAYSOFT.Core.Domain.Interfaces.Infrastructures.Data.Contexts;
+using Microsoft.EntityFrameworkCore;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BAYSOFT.Core.Domain.Services.Tests.Default.Samples
+{
+    internal static class AddMockedSamplesExtensions
+    {
+        private static IQueryable<Sample> GetSamplesCollection()
+        {
+            return new List<Sample> {
+                new Sample { },
+                new Sample { },
+            }.AsQueryable();
+        }
+
+        private static Mock<DbSet<Sample>> GetMockedDbSetSamples()
+        {
+            var samplesCollection = GetSamplesCollection();
+
+            var mockedDbSetSamples = samplesCollection.MockDbSet();
+
+            return mockedDbSetSamples;
+        }
+
+        internal static Mock<IDefaultDbContext> AddMockedSamples(this Mock<IDefaultDbContext> mockedDbContext)
+        {
+            var mockedDbSetSamples = GetMockedDbSetSamples();
+
+            mockedDbContext
+                .Setup(setup => setup.Samples)
+                .Returns(mockedDbSetSamples.Object);
+
+            return mockedDbContext;
+        }
+
+        internal static Mock<IDefaultDbContextQuery> AddMockedSamples(this Mock<IDefaultDbContextQuery> mockedDbContextQuery)
+        {
+            var mockedDbSetSamples = GetMockedDbSetSamples();
+
+            mockedDbContextQuery
+                .Setup(setup => setup.Samples)
+                .Returns(mockedDbSetSamples.Object);
+
+            return mockedDbContextQuery;
+        }
+    }
+}
