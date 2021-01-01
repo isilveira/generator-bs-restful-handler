81c9d0b85c88ea5a8c311009017b1fb26dae8982
Ítalo Silveira <italo-silveira@impulso.network>
2021-01-01 18:54:27 -0300

    remove template

--- generators/app/templates/src/BAYSOFT.Core.Domain/Interfaces/Services/Default/Samples/IPutSampleService.cs
+++
@@ -1,8 +0,0 @@
-﻿using <%= _ProjectName %>.Core.Domain.Entities.<%= _Context %>;
-
-namespace <%= _ProjectName %>.Core.Domain.Interfaces.Services.<%= _Context %>.<%= _Collection %>
-{
-    public interface IPut<%= _Entity %>Service : IDomainService<<%= _Entity %>>
-    {
-    }
-}
